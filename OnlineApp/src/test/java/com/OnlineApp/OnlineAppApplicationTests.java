e1193fd52c49af37b9cbe77f3b278feff6db9fc8
Rokesh <rokesh3901@gmail.com>
2025-01-22 11:07:10 +0530

    First commit

---
+++ OnlineApp/src/test/java/com/OnlineApp/OnlineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnlineApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
